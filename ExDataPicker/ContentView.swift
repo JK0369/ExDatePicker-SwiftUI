b44beb6cbcf354bab279286094e7c8972f22b5da
jake <palatable7@naver.com>
2022-08-14 01:56:52 +0900

    Implement

--- ExDataPicker/ContentView.swift
+++ ExDataPicker/ContentView.swift
@@ -8,14 +8,67 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+  @State var date = Date()
+  @State var color = Color(.sRGB, red: 0.98, green: 0.9, blue: 0.2)
+  
+  var dateRange: ClosedRange<Date> {
+    let min = Calendar.current.date(
+      byAdding: .year,
+      value: -10,
+      to: date
+    )!
+    let max = Calendar.current.date(
+      byAdding: .year,
+      value: 10,
+      to: date
+    )!
+    return min...max
+  }
+    
+  var body: some View {
+//    ScrollView {
+//      DatePicker(
+//        "DatePicker",
+//        selection: $date,
+//        in: dateRange,
+//        displayedComponents: [.date, .hourAndMinute]
+//      )
+//        .datePickerStyle(.automatic)
+//
+//      DatePicker(
+//        "DatePicker",
+//        selection: $date,
+//        in: dateRange,
+//        displayedComponents: [.date, .hourAndMinute]
+//      )
+//        .datePickerStyle(.graphical)
+//
+//      DatePicker(
+//        "DatePicker",
+//        selection: $date,
+//        in: dateRange,
+//        displayedComponents: [.date, .hourAndMinute]
+//      )
+//        .datePickerStyle(.wheel)
+//
+//      DatePicker(
+//        "DatePicker",
+//        selection: $date,
+//        in: dateRange,
+//        displayedComponents: [.date, .hourAndMinute]
+//      )
+//        .datePickerStyle(.compact)
+//    }
+    ColorPicker(
+      "ColorPicker",
+      selection: $color
+    )
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
+
